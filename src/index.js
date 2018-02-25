ac1e6245310213b93be77795daeaef07658c6fdb
WolfVic <vic16@hotmail.be>
2018-02-25 15:29:49 +0100

    ajout react-snap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement)
+} else {
+  ReactDOM.render(<App />, rootElement)
+}
+registerServiceWorker()
