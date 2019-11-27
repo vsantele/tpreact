b0d03e1bc9607d4297fb288324b7753ec2eb4c7f
Victor Santelé <Vic16@hotmail.be>
2019-11-27 11:55:18 +0100

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  // ReactDOM.render(<App />, div);
+  if (div.hasChildNodes()) {
+    ReactDOM.hydrate(<BrowserRouter><App /></BrowserRouter>, div)
+  } else {
+    ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div)
+  }
 });
